7d41870d5d90b49b9f11cb260030bbf273df4083
Hoang Viet Anh <anhhvph31156@fpt.edu.vn>
2023-11-24 10:48:09 +0700

    Dev 2 them hoa don cho, gio hang

---
+++ Test_Source_Tree/ConsoleApp2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hoa don cho");
+
+            Console.WriteLine("Gio Hang");
+        }
+    }
+}
